d8eb671fa5968feddbe99aaeed5de8c72ea50307
Ash <116465960+BunnehHewd@users.noreply.github.com>
2022-12-12 21:51:42 -0500

    Improved interactivity, added reset

--- apps.js
+++ apps.js
@@ -1,16 +1,19 @@
 let box = document.getElementById('container');
 const size = document.querySelector('.size');
+
+
 let value = 16; //Initial grid size
 const theme = document.querySelectorAll('button[type="button"]');
+let bool = true;
 
 // Color arrays
 const warmSelect = ['#F5C1Cf', '#FFD9E5', '#F8FFDE', '#FFEFC9', '#FFCFB0'];
+const coldSelect = ['#DDF2F4', '#84A6D6', '#4382BB', '#E4CEE0', '#A15D98'];
+const greySelect = ['#AEAEAE', '#949494', '#5C5E60', '#5D6D7E', '#97978F'];
 let index = 0;
-
+let change = "";
 
 let currentcolor = "black";
-let currenttheme = "black";
-
 
 function removeChildren(box) {
     while (box.firstChild) { // While there is a child, remove that child
@@ -24,8 +27,6 @@ function removeChildren(box) {
 function createGrid() {
     removeChildren(box);
 
-    const warmRand = warmSelect[Math.floor(Math.random() * warmSelect.length)];
-
     for (i = 0; i < value * value; i++) {
         var div = document.createElement("div");
         div.classList.add("cell");
@@ -33,20 +34,19 @@ function createGrid() {
     }
     box.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
     box.style.gridTemplaterows = `repeat(${value}, 1fr)`;
-    let cell = document.querySelectorAll('.cell');
 
-    cell.forEach(cell => {
-        cell.addEventListener('mouseover', () => {
-            coloring(cell);
-            cell.addEventListener('mouseover', () => {
-                indexUp();
-            })
-            
-            
-            console.log("testing");
-        })
+    let cell = document.querySelectorAll('.cell');
+    
+    cell.forEach(el => {
+        el.addEventListener('mouseover', () => {
+            colorSquare(el);
+            indexUp();
+           // coloring(el, `${index}`); 
+            console.log("in");            
+        });
     });
 
+    
     size.textContent = `${value} X ${value}`;
 }
 
@@ -57,8 +57,7 @@ function indexUp () {
     } else {
         index = 0;
     }
-
-    currentcolor = warmSelect[`${index}`];
+    console.log(index);
 }
 
 // -------Events
@@ -67,67 +66,49 @@ function indexUp () {
 // Event for changes
 document.addEventListener('input', function (e) {
     value = document.getElementById('grid').value;
-    console.log(value);
     createGrid();
 });
 
 
 
-function coloring(cell) {
-    cell.setAttribute('style', `background-color: ${currentcolor}`);
-
-    theme.forEach(button => {
-        button.addEventListener('click', () =>{
-            if(button.classList.contains('pastelWarm')) {
-
-                currentcolor = warmSelect[`${index}`];
-                console.log(currentcolor);
-            } else if(button.classList.contains('pastelCold')) {
-                currentcolor = "blue";
-            } else if(button.classList.contains('greyscale')) {
-                currentcolor = "grey";
-            } else if(button.classList.contains('black')) {
-                currentcolor = "black";
-            } else {
-                currentcolor = "yellow";
-            }
-        })
+theme.forEach(button => {
+    button.addEventListener('click', () =>{
+        if(button.classList.contains('pastelWarm')) {  
+            change = "warm";
+        } else if(button.classList.contains('pastelCold')) {
+            change = "cold";
+        } else if(button.classList.contains('greyscale')) {
+            change = "grey";
+        } else if(button.classList.contains('black')) {
+            change = "black";
+        } else if(button.classList.contains('rainbow')) {
+            change = "rainbow";
+        } else {
+            createGrid();
+        }
     })
+})
+
+
+function colorSquare(el) {
+    indexUp();
+    
+    if (change === "warm") {
+        currentcolor =  warmSelect[`${index}`];
+    } else if (change === "cold") {
+        currentcolor =  coldSelect[`${index}`];
+    } else if (change === "grey") {
+        currentcolor =  greySelect[`${index}`];
+    } else if (change === "black") {
+        currentcolor = "black";
+    } else if (change === "rainbow") {
+        currentcolor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    el.setAttribute('style', `background-color: ${currentcolor}`);
+    
 }
 
 
+console.log(index);
 
-
-/*
-
-function color(e) {
-    c 
-    e.setAttribute('style', 'background-color: black'); 
-
-
-    theme.forEach(button => {
-        button.addEventListener('click', () => {
-            e.addEventListener('mouseover', () => {
-                if(button.classList.contains('pastelWarm')){
-                    e.target.style = 'background-color: red';
-                    console.log("enter");        
-                } else {
-                   //  
-                    console.log("why");
-                }
-            })
-        })
-    });
-
-}
-
-
-
-function colors() {
-   
-       
-}
-
-*/
 createGrid();
-//colors();
